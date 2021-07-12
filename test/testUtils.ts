4ba1da7a7d240dbcea9bfac7747f43e11c75655b
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2021-07-12 22:28:02 +0200

    chore: format and lint

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,21 +1,22 @@
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
+import { ReactElement } from 'react'
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
 
 const Providers = ({ children }) => {
-  return children
-  // return (
-  //   <ThemeProvider theme="light">
-  //     <TranslationProvider messages={defaultStrings}>
-  //       {children}
-  //     </TranslationProvider>
-  //   </ThemeProvider>
-  // )
+    return children
+    // return (
+    //   <ThemeProvider theme="light">
+    //     <TranslationProvider messages={defaultStrings}>
+    //       {children}
+    //     </TranslationProvider>
+    //   </ThemeProvider>
+    // )
 }
 
-const customRender = (ui, options = {}) =>
-  render(ui, { wrapper: Providers, ...options })
+const customRender = (ui: ReactElement, options = {}): RenderResult =>
+    render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
 export * from '@testing-library/react'
